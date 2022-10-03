c25b330b601d5ee38349d326e79c18b00ed776c4
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2022-10-04 03:03:31 +0500

    Updated the menus

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -51,16 +51,16 @@ export default function Nav() {
               <div className="overflow-hidden overflow-y-auto max-h-[75vh] scrollbar-y">
                 <div className="flex flex-col gap-x-0 mt-5 divide-y divide-dashed divide-gray-200 md:flex-row md:items-center md:justify-end md:gap-x-7 md:mt-0 md:pl-7 md:divide-y-0 md:divide-solid dark:divide-gray-700">
                   <a
-                    className="font-medium text-blue-600 py-3 md:py-6 dark:text-blue-500"
+                    className="font-medium text-red-600 py-3 md:py-6 dark:text-red-500"
                     href="#"
                     aria-current="page"
                   >
-                    Landing
+                    Home
                   </a>
                   <Menu>
                     <div className="hs-dropdown py-3 md:py-6">
-                      <Menu.Button className="flex items-center w-full text-gray-800 hover:text-gray-600 font-medium dark:text-gray-200 dark:hover:text-gray-500">
-                        Company
+                      <Menu.Button className="flex items-center w-full text-gray-800 hover:text-red-600 font-medium dark:text-gray-200 dark:hover:text-gray-500">
+                        About
                         <svg
                           className="ml-2 w-2.5 h-2.5 text-gray-600"
                           width={16}
@@ -94,31 +94,9 @@ export default function Nav() {
                             </svg>
                             <div className="grow">
                               <span className="block font-semibold mb-1 text-gray-800 dark:text-gray-200">
-                                Data
+                                Servers
                               </span>
-                              How you get the most accurate and up-to-date data
-                            </div>
-                          </Menu.Item>
-                          <Menu.Item as="a" className="inline-flex gap-x-5 w-full p-4 text-gray-600 rounded-md hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                            href="#">
-                            <svg
-                              className="flex-shrink-0 w-5 h-5 mt-1"
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={16}
-                              height={16}
-                              fill="currentColor"
-                              viewBox="0 0 16 16"
-                            >
-                              <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
-                            </svg>
-                            <div className="grow">
-                              <span className="block font-semibold mb-1 text-gray-800 dark:text-gray-200">
-                                Team{" "}
-                                <span className="inline ml-1 text-xs bg-blue-600 text-white py-1 px-2 rounded-full">
-                                  We're hiring
-                                </span>
-                              </span>
-                              Meet the people building products to help your business grow
+                              Check out the list of Nationchat Servers
                             </div>
                           </Menu.Item>
                           <Menu.Item as="a" className="inline-flex gap-x-5 w-full p-4 text-gray-600 rounded-md hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
@@ -136,9 +114,31 @@ export default function Nav() {
                             </svg>
                             <div className="grow">
                               <span className="block font-semibold mb-1 text-gray-800 dark:text-gray-200">
-                                Blog
+                                Staff
                               </span>
-                              The latest news, feature releases, and how to grow with data
+                              The Staff of NationCHAT IRC Network
+                            </div>
+                          </Menu.Item>
+                          <Menu.Item as="a" className="inline-flex gap-x-5 w-full p-4 text-gray-600 rounded-md hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
+                            href="#">
+                            <svg
+                              className="flex-shrink-0 w-5 h-5 mt-1"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width={16}
+                              height={16}
+                              fill="currentColor"
+                              viewBox="0 0 16 16"
+                            >
+                              <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+                            </svg>
+                            <div className="grow">
+                              <span className="block font-semibold mb-1 text-gray-800 dark:text-gray-200">
+                                Team{" "}
+                                <span className="inline ml-1 text-xs bg-red-600 text-white py-1 px-2 rounded-full">
+                                  We're hiring
+                                </span>
+                              </span>
+                              Meet the people opearting the network on daily basis.
                             </div>
                           </Menu.Item>
                         </div>
@@ -191,17 +191,17 @@ export default function Nav() {
                     </Menu.Items>
                   </Menu> */}
                   <a
-                    className="font-medium text-gray-800 hover:text-gray-600 py-3 md:py-6 dark:text-gray-200 dark:hover:text-gray-500"
+                    className="font-medium text-gray-800 hover:text-red-600 py-3 md:py-6 dark:text-gray-200 dark:hover:text-gray-500"
                     href="#"
                   >
-                    Work
+                    News
                   </a>
                   <div className="pt-3 md:pt-0">
                     <a
-                      className="inline-flex justify-center items-center gap-x-2 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-2.5 px-3 dark:focus:ring-offset-gray-800"
+                      className="inline-flex justify-center items-center gap-x-2 text-center bg-red-600 hover:bg-red-700 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-2.5 px-3 dark:focus:ring-offset-gray-800"
                       href="#"
                     >
-                      Request demo
+                      Contact
                     </a>
                   </div>
                 </div>
